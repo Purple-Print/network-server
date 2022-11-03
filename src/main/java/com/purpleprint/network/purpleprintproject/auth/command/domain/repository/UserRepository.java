020fc4cce8b8837666dea39ba07b5901102f35e6
hyunjungjeon <sksznddl574@gmail.com>
2022-11-03 16:43:17 +0900

    feature : #19 User Repository 생성

---
+++ src/main/java/com/purpleprint/network/purpleprintproject/auth/command/domain/repository/UserRepository.java
@@ -0,0 +1,26 @@
+package com.purpleprint.network.purpleprintproject.auth.command.domain.repository;
+
+import com.purpleprint.network.purpleprintproject.auth.command.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <pre>
+ * Class : UserRepository
+ * Comment: User repository
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-01       전현정           최초 생성
+ * </pre>
+ *
+ * @author 전현정(최초 작성자)
+ * @version 1(클래스 버전)
+ * @see
+ */
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer> {
+
+    User findByUsername(String username);
+}
