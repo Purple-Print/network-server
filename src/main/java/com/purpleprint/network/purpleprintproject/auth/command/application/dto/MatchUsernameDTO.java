83ea564d375d64119575443cd2b707898f8d922f
hyunjungjeon <sksznddl574@gmail.com>
2022-11-04 13:59:21 +0900

    feature : #26 아이디 찾기 DTO 생성

---
+++ src/main/java/com/purpleprint/network/purpleprintproject/auth/command/application/dto/MatchUsernameDTO.java
@@ -0,0 +1,37 @@
+package com.purpleprint.network.purpleprintproject.auth.command.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
+/**
+ * <pre>
+ * Class : MatchUsernameDTO
+ * Comment: 아이디 찾기
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-04       전현정           최초 생성
+ * </pre>
+ *
+ * @author 전현정(최초 작성자)
+ * @version 1(클래스 버전)
+ * @see
+ */
+@Data   //lombok 사용시 class의 모든 필드에 대한 getter/setter/toString/equals 와 같은 함수 사용 가능
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 만듦
+@NoArgsConstructor  // 파라미터가 없는 기본 생성자를 생성
+public class MatchUsernameDTO {
+
+    @NotBlank(message = "이름은 필수 입력 값입니다.")
+    private String name;
+
+    @NotBlank(message = "email은 필수 입력 값입니다.")
+    @Email
+    private String email;
+
+}
