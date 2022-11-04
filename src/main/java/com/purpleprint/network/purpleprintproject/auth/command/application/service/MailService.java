37f9fe232c3a39ba01fee0becc4389baa504aa8c
hyunjungjeon <sksznddl574@gmail.com>
2022-11-04 15:36:46 +0900

    feature : #26 비밀번호 찾기 임시 비밀번호 이메일 전송 구현

--- src/main/java/com/purpleprint/network/purpleprintproject/auth/command/application/service/MailService.java
+++ src/main/java/com/purpleprint/network/purpleprintproject/auth/command/application/service/MailService.java
@@ -200,4 +200,79 @@ public class MailService {
 
         System.out.println("메일 발송 완료");
     }
+
+    @Async
+    public void sendEmailForMatchPassword(String toEmail, String tempPassword) throws MessagingException {
+
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true);
+
+        helper.setFrom(email);
+        helper.setTo(toEmail);
+
+        helper.setSubject("[필로칼로] 비밀번호 찾기 안내");
+        helper.setText("text/html","<!DOCTYPE html>\n" +
+                "<html lang=\"en\" xmlns=\"http://www.w3.org/1999/xhtml\" xmlns:o=\"urn:schemas-microsoft-com:office:office\">\n" +
+                "<head>\n" +
+                "  <meta charset=\"utf-8\">\n" +
+                "  <meta name=\"viewport\" content=\"width=device-width,initial-scale=1\">\n" +
+                "  <meta name=\"x-apple-disable-message-reformatting\">\n" +
+                "  <title></title>\n" +
+                "\n" +
+                "\n" +
+                "</head>\n" +
+                "<body style=\"margin:0;padding:0;word-spacing:normal;background-color:#939297;\">\n" +
+                "  <div role=\"article\" aria-roledescription=\"email\" lang=\"en\" style=\"text-size-adjust:100%;-webkit-text-size-adjust:100%;-ms-text-size-adjust:100%;background-color:#939297;\">\n" +
+                "    <table role=\"presentation\" style=\"width:100%;border:none;border-spacing:0;\">\n" +
+                "      <tr>\n" +
+                "        <td align=\"center\" style=\"padding:0;\">\n" +
+                "\n" +
+                "          <table role=\"presentation\" style=\"width:94%;max-width:600px;border:none;border-spacing:0;text-align:left;font-family:Arial,sans-serif;font-size:16px;line-height:22px;color:#363636;\">\n" +
+                "            <tr>\n" +
+                "              <td style=\"padding:40px 30px 30px 30px;text-align:center;font-size:24px;font-weight:bold;\">\n" +
+                "                <a href=\"https://ibb.co/YQfBKss\"><img src=\"\" alt=\"\" border=\"0\"></a>\n" +
+                "            </td>\n" +
+                "            </tr>\n" +
+                "            <tr>\n" +
+                "              <td style=\"padding:30px;background-color:#ffffff;\">\n" +
+                "                <!--title-->\n" +
+                "                <h1 align=\"center\" style=\"margin-top:0;margin-bottom:16px;font-size:26px;line-height:32px;font-weight:bold;letter-spacing:-0.02em;\">필로칼로을 이용해 주셔서 감사합니다.</h1>\n" +
+                "                <!--first paragraph-->\n" +
+                "                <p style=\"margin:0;\"></p>\n" +
+                "              </td>\n" +
+                "            </tr>\n" +
+                "\n" +
+                "            <tr>\n" +
+                "              <td align=\"center\" style=\"padding:35px 30px 11px 30px;font-size:0;background-color:#ffffff;border-bottom:1px solid #f0f0f5;border-color:rgba(201,201,207,.35);\">\n" +
+                "\n" +
+                "                <!-- <div class=\"col-sml\" style=\"display:inline-block;width:100%;max-width:145px;vertical-align:top;text-align:left;font-family:Arial,sans-serif;font-size:14px;color:#363636;\">\n" +
+                "                  <img src=\"\" width=\"115\" alt=\"\" style=\"width:115px;max-width:80%;margin-bottom:20px;\">\n" +
+                "                </div> -->\n" +
+                "\n" +
+                "                <div  class=\"col-lge\" style=\"display:inline-block;width:100%;max-width:395px;vertical-align:top;padding-bottom:20px;font-family:Arial,sans-serif;font-size:16px;line-height:22px;color:#363636;\">\n" +
+                "                    <p style=\"margin-top:0;margin-bottom:18px;\"></p>\n" +
+                "                  <p style=\"margin:0;\"><div href=\"https://example.com/\" style=\"background: #8977AD; text-decoration: none; padding: 10px 25px; color: #ffffff; border-radius: 4px; display:inline-block; mso-padding-alt:0;text-underline-color:#ff3884\"><!--[if mso]><i style=\"letter-spacing: 25px;mso-font-width:-100%;mso-text-raise:20pt\">&nbsp;</i><![endif]--><span style=\"mso-text-raise:10pt;font-weight:bold;\">"+tempPassword+"</span><!--[if mso]><i style=\"letter-spacing: 25px;mso-font-width:-100%\">&nbsp;</i><![endif]--></div></p>\n" +
+                "                </div>\n" +
+                "              </td>\n" +
+                "            </tr>\n" +
+                "\n" +
+                "            <p>변경된 비밀번호 입니다. \n  로그인 후 비밀번호를 변경해주세요</p> \n" +
+                "\n" +
+                "            <tr>\n" +
+                "              <td style=\"padding:30px;text-align:center;font-size:12px;background-color:#404040;color:#cccccc;\">\n" +
+                "                <p style=\"margin:0;font-size:14px;line-height:20px;\">Copyright 2022. purpleprint all rights reserved.<br></p>\n" +
+                "              </td>\n" +
+                "            </tr>\n" +
+                "          </table>\n" +
+                "\n" +
+                "        </td>\n" +
+                "      </tr>\n" +
+                "    </table>\n" +
+                "  </div>\n" +
+                "</body>\n" +
+                "</html>");
+        mailSender.send(message);
+
+        System.out.println("메일 발송 완료");
+    }
 }
