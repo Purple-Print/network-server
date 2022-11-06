22ac2b00edf3cd72475f8751d6442f60c9c5f91d
sanghak <yee950419@naver.com>
2022-11-06 22:49:10 +0900

    fix : #31 Heart 레포지토리 수정

--- src/main/java/com/purpleprint/network/purpleprintproject/heart/command/domain/repository/HeartRepository.java
+++ src/main/java/com/purpleprint/network/purpleprintproject/heart/command/domain/repository/HeartRepository.java
@@ -1,8 +1,8 @@
 package com.purpleprint.network.purpleprintproject.heart.command.domain.repository;
 
-import com.purpleprint.network.purpleprintproject.heart.command.application.dto.HeartDTO;
 import com.purpleprint.network.purpleprintproject.heart.command.domain.model.Heart;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Date;
