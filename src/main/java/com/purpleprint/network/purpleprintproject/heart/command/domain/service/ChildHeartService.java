090e84b69d44d52e5324a37b7ad4ccf039fe3bd6
sanghak <yee950419@naver.com>
2022-11-06 16:03:06 +0900

    feature : #31 하트 나눠주기 외부 service 생성

---
+++ src/main/java/com/purpleprint/network/purpleprintproject/heart/command/domain/service/ChildHeartService.java
@@ -0,0 +1,20 @@
+package com.purpleprint.network.purpleprintproject.heart.command.domain.service;
+
+/**
+ * <pre>
+ * Class : ChildHeartService
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-06       이상학           최초 생성
+ * </pre>
+ *
+ * @author 이상학(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+
+public interface ChildHeartService {
+    public int giveHeart(int id);
+}
