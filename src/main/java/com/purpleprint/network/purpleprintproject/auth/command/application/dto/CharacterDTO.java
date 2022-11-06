c55d5bf231dd88399fd7b0222ba289f8dc519f56
hyunjungjeon <sksznddl574@gmail.com>
2022-11-06 21:24:15 +0900

    feature : #34 characterDTO 생성

---
+++ src/main/java/com/purpleprint/network/purpleprintproject/auth/command/application/dto/CharacterDTO.java
@@ -0,0 +1,31 @@
+package com.purpleprint.network.purpleprintproject.auth.command.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * <pre>
+ * Class : CharacterDTO
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-06       전현정           최초 생성
+ * </pre>
+ *
+ * @author 전현정(최초 작성자)
+ * @version 1(클래스 버전)
+ * @see
+ */
+
+@Data   //lombok 사용시 class의 모든 필드에 대한 getter/setter/toString/equals 와 같은 함수 사용 가능
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 만듦
+@NoArgsConstructor  // 파라미터가 없는 기본 생성자를 생성
+public class CharacterDTO {
+
+    private int characterId;
+    private String url;
+    private String fileName;
+}
