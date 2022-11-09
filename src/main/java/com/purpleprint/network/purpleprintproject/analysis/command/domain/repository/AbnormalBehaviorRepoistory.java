5642bbb054ebe45233bea06aa4af154fa506044e
hyunjungjeon <sksznddl574@gmail.com>
2022-11-09 13:25:41 +0900

    feature : #60 이상행동 repository 생성

---
+++ src/main/java/com/purpleprint/network/purpleprintproject/analysis/command/domain/repository/AbnormalBehaviorRepoistory.java
@@ -0,0 +1,24 @@
+package com.purpleprint.network.purpleprintproject.analysis.command.domain.repository;
+
+import com.purpleprint.network.purpleprintproject.analysis.command.domain.model.AbnormalBehavior;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+/**
+ * <pre>
+ * Class : AbnormalBehaviorRepoistory
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-09       전현정           최초 생성
+ * </pre>
+ *
+ * @author 전현정(최초 작성자)
+ * @version 1(클래스 버전)
+ * @see
+ */
+@Repository
+public interface AbnormalBehaviorRepoistory extends JpaRepository<AbnormalBehavior, Integer> {
+}
