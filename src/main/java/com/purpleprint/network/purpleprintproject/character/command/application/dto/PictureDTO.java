406337fb12ce6ad49a25d33be357b3cc04ca6b21
sanghak <yee950419@naver.com>
2022-11-10 20:58:07 +0900

    feature : #41 PictureDTO 추가

---
+++ src/main/java/com/purpleprint/network/purpleprintproject/character/command/application/dto/PictureDTO.java
@@ -0,0 +1,28 @@
+package com.purpleprint.network.purpleprintproject.character.command.application.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * <pre>
+ * Class : PictureDTO
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-09       이상학           최초 생성
+ * </pre>
+ *
+ * @author 이상학(최초 작성자)
+ * @version 1(클래스 버전)
+ */
+@Data   //lombok 사용시 class의 모든 필드에 대한 getter/setter/toString/equals 와 같은 함수 사용 가능
+@AllArgsConstructor // 모든 필드 값을 파라미터로 받는 생성자를 만듦
+@NoArgsConstructor  // 파라미터가 없는 기본 생성자를 생성
+public class PictureDTO {
+
+    MultipartFile imageFile;
+}
