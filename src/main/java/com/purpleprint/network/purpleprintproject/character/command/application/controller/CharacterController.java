a75397c20fa5b2f3d0ac9e6809f3ff9859e41b9b
sanghak <yee950419@naver.com>
2022-11-10 20:57:08 +0900

    feature : #41 캐릭터 추천 controller 임시 구현

--- src/main/java/com/purpleprint/network/purpleprintproject/character/command/application/controller/CharacterController.java
+++ src/main/java/com/purpleprint/network/purpleprintproject/character/command/application/controller/CharacterController.java
@@ -1,20 +1,30 @@
 package com.purpleprint.network.purpleprintproject.character.command.application.controller;
 
 import com.purpleprint.network.purpleprintproject.character.command.application.dto.CharacterDTO;
+import com.purpleprint.network.purpleprintproject.character.command.application.dto.PictureDTO;
+import com.purpleprint.network.purpleprintproject.character.command.application.exception.PictureReceiveFailException;
 import com.purpleprint.network.purpleprintproject.character.command.application.service.CharacterService;
 import com.purpleprint.network.purpleprintproject.common.dto.UserDTO;
 import com.purpleprint.network.purpleprintproject.common.responsemessage.ResponseMessage;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.core.io.ByteArrayResource;
+import org.springframework.http.*;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartFile;
 
+import javax.print.attribute.standard.Media;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.nio.charset.Charset;
+import java.util.Base64;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -61,4 +71,56 @@ public class CharacterController {
                     .body(new ResponseMessage(HttpStatus.BAD_REQUEST, "캐릭터 생성 실패", responseMap));
         }
     }
+
+    private static byte[] getFileBinary(String filepath) {
+        File file = new File(filepath);
+        byte[] data = new byte[(int) file.length()];
+        try (FileInputStream stream = new FileInputStream(file)) {
+            stream.read(data, 0, data.length);
+        } catch (Throwable e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+    @PostMapping("/recommend")
+    public ResponseEntity<?> recommendCharacter(@AuthenticationPrincipal UserDTO userDTO, PictureDTO pictureDTO) throws IOException {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.MULTIPART_FORM_DATA);
+
+        String imageData = Base64.getEncoder().encodeToString(pictureDTO.getImageFile().getBytes());
+
+
+
+//        byte[] binary = pictureDTO.getImageFile().getBytes();
+
+        System.out.println("imageData : " + imageData);
+
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("imageFile", imageData);
+
+        HttpEntity<?> requestEntity = new HttpEntity<>(body, headers);
+
+        String serverURL = "http://192.168.0.184:8080/auth/test";
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> response = restTemplate.postForEntity(serverURL, requestEntity, String.class);
+
+
+//        byte[] resultImage = Files.readAllBytes(new File("\\public\\"+fileName).toPath());
+        System.out.println("response : " + response);
+//
+//        return response;
+//        String recommendInfo = characterService.recommendCharacter(userDTO, pictureDTO.getImageFile());
+//
+        Map<String, Object> responseMap = new HashMap<>();
+
+        responseMap.put("recommendInfo"," recommendInfo");
+
+        return ResponseEntity
+                .ok()
+                .body(new ResponseMessage(HttpStatus.OK, "사진 업로드 성공", responseMap));
+
+    }
+
 }
