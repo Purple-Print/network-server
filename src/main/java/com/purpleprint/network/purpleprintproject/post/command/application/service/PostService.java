4d6a414774f9bbb5764b917ec7a923441a503b9d
hyunjungjeon <sksznddl574@gmail.com>
2022-11-11 07:49:47 +0900

    feature : #44 게시된 영상 조회 service 생성

---
+++ src/main/java/com/purpleprint/network/purpleprintproject/post/command/application/service/PostService.java
@@ -0,0 +1,50 @@
+package com.purpleprint.network.purpleprintproject.post.command.application.service;
+
+import com.purpleprint.network.purpleprintproject.common.dto.ChildDTO;
+import com.purpleprint.network.purpleprintproject.common.dto.UserDTO;
+import com.purpleprint.network.purpleprintproject.play.command.domain.model.Video;
+import com.purpleprint.network.purpleprintproject.post.command.application.exception.SavePostAndVideoFailException;
+import com.purpleprint.network.purpleprintproject.post.command.domain.model.Post;
+import com.purpleprint.network.purpleprintproject.post.command.domain.repository.PostRepository;
+import com.purpleprint.network.purpleprintproject.post.command.domain.service.PostingVideoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * <pre>
+ * Class : PostService
+ * Comment: 클래스에 대한 간단 설명
+ * History
+ * ================================================================
+ * DATE             AUTHOR           NOTE
+ * ----------------------------------------------------------------
+ * 2022-11-11       전현정           최초 생성
+ * </pre>
+ *
+ * @author 전현정(최초 작성자)
+ * @version 1(클래스 버전)
+ * @see
+ */
+
+@Service
+public class PostService {
+
+    private final PostRepository postRepository;
+    private final PostingVideoService postingVideoService;
+
+
+    @Autowired
+    public PostService(PostRepository postRepository, PostingVideoService postingVideoService) {
+        this.postRepository = postRepository;
+        this.postingVideoService = postingVideoService;
+    }
+    public List<Post> selectPostList() {
+
+        return postRepository.findAllByApprovalAndDeleteYn("Y", "N");
+    }
+}
